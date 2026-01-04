09e610d5ea7224e4cfc6f490fc8f1b347268df39
Qmm <2268428576@qq.com>
2026-01-04 12:40:46 +0800

    add self balance parse

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+use std::{collections::HashSet, str::FromStr};
 use {
     anyhow,
     borsh::BorshDeserialize,
@@ -28,6 +29,29 @@ use {
 pub mod confirm;
 pub use confirm::confirm_first::confirm_tx;
 pub use confirm::confirm_success::confirm_success_tx;
+
+// Key 为 (Owner, Mint)
+pub static SELF_TOKEN_BALANCE: LazyLock<whirlwind::ShardMap<(Pubkey, Pubkey), u64>> =
+    LazyLock::new(|| whirlwind::ShardMap::with_shards(16));
+
+pub async fn self_balance_of(owner: &Pubkey, mint: &Pubkey) -> u64 {
+    SELF_TOKEN_BALANCE
+        .get(&(*owner, *mint))
+        .await
+        .map(|res| *res)
+        .unwrap_or(0)
+}
+
+pub static MONITORED_PAYERS: LazyLock<RwLock<HashSet<Pubkey>>> =
+    LazyLock::new(|| RwLock::new(HashSet::new()));
+
+// 在 subscribe_nonce_and_transaction 开始时调用
+async fn set_monitored_payers(payers: &[Pubkey]) {
+    let mut lock = MONITORED_PAYERS.write().await;
+    for p in payers {
+        lock.insert(*p);
+    }
+}
 
 // 定义交易结果的全局广播 channel
 global_broadcast! {
@@ -147,6 +171,7 @@ pub async fn subscribe_nonce_and_transaction(
         init_nonce(*nonce_account).await;
         info!("Starting to monitor account: {}", nonce_account);
     }
+    set_monitored_payers(&payer_pubkeys[..]).await;
 
     let mut client = setup_client().await?;
     let mut subscribe_accounts = payer_pubkeys
@@ -234,6 +259,7 @@ pub async fn subscribe_nonce_and_transaction(
                     match &meta.status {
                         Ok(_) => {
                             info!("交易成功: {:?}", sig);
+                            update_balances_from_tx(&tx).await;
                             let event = tx_result_channel::TxResultEvent {
                                 signature: sig,
                                 tx,
@@ -280,4 +306,33 @@ async fn setup_client() -> Result<GeyserGrpcClient<impl Interceptor>, anyhow::Er
         .await?;
 
     Ok(client)
+}
+
+async fn update_balances_from_tx(tx: &TransactionFormat) {
+    let Some(meta) = &tx.meta else { return };
+    let Some(post_balances) = &meta.post_token_balances else {
+        return;
+    };
+
+    // 获取当前监控的名单
+    let monitored = MONITORED_PAYERS.read().await;
+
+    for tb in post_balances {
+        if let Ok(owner_pk) = Pubkey::from_str(&tb.owner) {
+            // 如果该 Owner 在我们的监控名单中
+            if monitored.contains(&owner_pk) {
+                if let Ok(mint_pk) = Pubkey::from_str(&tb.mint) {
+                    let amount = tb.ui_token_amount.amount.parse::<u64>().unwrap_or(0);
+
+                    info!(
+                        "[余额更新] 账户: {}, Mint: {}, 新余额: {}",
+                        owner_pk, mint_pk, amount
+                    );
+
+                    // 更新复合 Key 存储
+                    SELF_TOKEN_BALANCE.insert((owner_pk, mint_pk), amount).await;
+                }
+            }
+        }
+    }
 }
